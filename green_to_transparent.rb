7a0111604ea476703a3d2ff4c382a93cc43aad5f
Jason Denney <jasondenney@gmail.com>
2011-12-28 22:26:52 -0500

    can now merge a background

--- green_to_transparent.rb
+++ green_to_transparent.rb
@@ -3,12 +3,14 @@ require 'RMagick'
 require 'json'
 include Magick
 
-def remove_green(dir, filename)
+def remove_green(dir, filename, bg_dir, bg_fname)
   begin
     il = ImageList.new
     temp = ImageList.new
     base = ImageList.new(dir + "/" + filename)
-    #light 46f75f
+    bg = ImageList.new(bg_dir + "/" + bg_fname)
+
+        #light 46f75f
     light_green = '#46f75f'
     #mid 249523
     mid_green = '#249523'
@@ -19,13 +21,19 @@ def remove_green(dir, filename)
     #first pass
     base.fuzz = 1700
     temp << base.scale(0.25).opaque_channel(dark_green, mid_green)
+    puts temp.columns
+    puts temp.rows
+    #resize bg to picture
+    il << bg.resize_to_fill(temp.columns, temp.rows)
+
     #second pass
     second_pass = temp.flatten_images
     second_pass.fuzz = 12500
     il << second_pass.opaque_channel(mid_green, light_green).opaque_channel(light_green, light_green).transparent(light_green, Magick::TransparentOpacity)
     #final.write("test_out/thumbs_up.png")
     #puts il.inspect
-    il.display
+    final = il.flatten_images
+    final.display
   rescue
     puts "Caught exception: #{$!}"
     exit
@@ -34,13 +42,16 @@ end
 
 dir_name = "test_source"
 output_dir_name = "test_out"
+bg_dir = "backgrounds"
+=begin
 Dir.entries(dir_name).each do |filename|
   next if filename == "."
   next if filename == ".."
   remove_green(dir_name, filename)
 end
+=end
 
-#remove_green(dir_name, "thumbs_up.JPG")
+remove_green(dir_name,  "thumbs_up.JPG", bg_dir, "explosion.jpeg")
 exit
 
 
